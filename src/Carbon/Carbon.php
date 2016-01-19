bc3008ca873b57642d74f3873be11418dbce94fa
Hamed <iraitc@gmail.com>
2016-01-19 20:25:35 +0330

    push to dev

--- src/Carbon/Carbon.php
+++ src/Carbon/Carbon.php
@@ -4,7 +4,7 @@
  * This file is part of the Carbon package.
  *
  * (c) Brian Nesbitt <brian@nesbot.com>
- *
+ * Cloned by larabook team
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
